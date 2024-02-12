b667d94ec294a46a4212f6d783b9e08ef986f767
ChrisxLiz <chrislizi@gmail.com>
2024-02-12 11:35:56 -0500

    añadiendo estilos

--- front/src/components/Inicio.jsx
+++ front/src/components/Inicio.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
 import { Link } from 'react-router-dom';
 
 export const Inicio = () => {
     return (
-        <div>
-            <nav>
-                <ul>
-                    <li><Link to="/login">Iniciar sesión</Link></li>
-                    <li><Link to="/registro">Registro</Link></li>
+        <div className="container mx-auto px-4">
+            <nav className="py-4">
+                <ul className="flex">
+                    <li className="mr-4"><Link to="/login" className="text-blue-500 hover:text-blue-700">Iniciar sesión</Link></li>
+                    <li><Link to="/registro" className="text-blue-500 hover:text-blue-700">Registro</Link></li>
                 </ul>
             </nav>
-            <h1>Bienvenido a la aplicación de citas médicas</h1>
+            <h1 className="text-3xl font-bold mb-4">Bienvenido a la aplicación de citas médicas</h1>
             {/* Otro contenido de la página de inicio */}
         </div>
     );
 }
+
